43d173153350024321f447169334b4536400e65d
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-11-02 13:04:40 -0400

    set up route for getting restaurants

--- crudyrestaurants_initial/src/main/java/com/lambdaschool/crudyrestaurants/services/RestaurantServicesImpl.java
+++ crudyrestaurants_initial/src/main/java/com/lambdaschool/crudyrestaurants/services/RestaurantServicesImpl.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Transactional
 @Service(value = "restaurantService")
 public class RestaurantServicesImpl  implements RestaurantServices {
@@ -18,5 +21,33 @@ public class RestaurantServicesImpl  implements RestaurantServices {
     @Override
     public Restaurant save(Restaurant restaurant) {
         return restaurantRepos.save(restaurant);
+    }
+
+    @Override
+    public List<Restaurant> findAllRestaurants() {
+        List<Restaurant> list = new ArrayList<>();
+        restaurantRepos.findAll().iterator().forEachRemaining(list::add);
+
+        return list;
+    }
+
+    @Override
+    public Restaurant findRestaurantById(long id) {
+        return null;
+    }
+
+    @Override
+    public Restaurant findRestaurantByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Restaurant> findByNameLike(String subname) {
+        return null;
+    }
+
+    @Override
+    public List<Restaurant> findByDishLike(String subdish) {
+        return null;
     }
 }
